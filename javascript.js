7c5cac09cc71a1a5995d66d250ff58ea81755169
JoeyWeixel <joeyweixel@gmail.com>
2022-05-01 23:57:22 -0500

    Add working change grid size

--- javascript.js
+++ javascript.js
@@ -1,23 +1,45 @@
 function createGrid(n){
     let grid = document.querySelector('.grid');
+    removeChildren(grid);
     for(let i=0; i<n; i++){
         let row = document.createElement('div');
-        //square.setAttribute('id', 'square' + i)
+        let side = 100/n;
+        row.style.height = `${side}` + '%'
         row.classList.add('row');
         for(let j=0; j<n; j++){
             let square = document.createElement('div');
             square.classList.add('square');
+            console.log(`${side}` + '%')
+            square.style.width = `${side}` + '%'
             row.appendChild(square);
         }
         grid.appendChild(row);
     }
+    const squares = document.querySelectorAll('.square');
+    squares.forEach(square => square.addEventListener('mouseover', hover));
+    return;
 }
 
 function hover(e){
     this.classList.add('hover');
 }
 
+function removeChildren(parent){
+    while(parent.firstChild){
+        parent.removeChild(parent.firstChild);
+    }
+    return;
+}
+
+function newGrid(){
+    let n = prompt("Enter new grid size");
+    if(n>100 || n<=0) return;
+    createGrid(n);
+    return;
+}
+
 createGrid(16);
 
-const squares = document.querySelectorAll('.square');
-squares.forEach(square => square.addEventListener('mouseover', hover));
+const button = document.querySelector('button');
+button.addEventListener('click', newGrid);
+
